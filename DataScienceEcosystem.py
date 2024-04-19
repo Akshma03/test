6a9d81d0c2f5518b0c537a562c4e4fffe343d432
Akshma03 <167538552+Akshma03@users.noreply.github.com>
2024-04-19 18:42:37 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# Exercise 2: Create markdown cell with title of the notebook
+
+# <h1>Data Science Tools and Ecosystem</h1>
+
+# Exercise 3 - Create a markdown cell for an introduction. (1 pt)
+
+# <p>In this notebook, Data Science Tools and Ecosystem are summarized.</p>
+
+# Exercise 4 - Create a markdown cell to list data science languages
+
+# <p>Some of the popular languages that Data Scientists use are:</p>
+# <ol>
+#     <li>Python</li>
+#     <li>R</li>
+#     <li>SQL</li>
+#     <li>Julia</li>
+#     <li>Scala</li>
+#     
+# </ol>
+# 
+
+# Exercise 5 - Create a markdown cell to list data science libraries
+
+# <p>Some of the commonly used libraries used by Data Scientists include:</p>
+# <ol>
+#     <li>NumPy</li>
+#     <li>SciPy</li>
+#     <li>Pandas</li>
+#     <li>Matplotlib</li>
+#     <li>TensorFlow</li>
+# </ol>
+#     
+
+# Exercise 6 - Create a markdown cell with a table of Data Science tools
+
+# <table>
+#     <tr><th>Data Science Tools</th></tr>
+#     <tr><td>Tableau</td></tr>
+#     <tr><td>Pandas</td></tr>
+#     <tr><td>Apache Spark</td></tr>
+#     <tr><td>Matlab</td></tr>
+#     <tr><td>PyTorch</td></tr>
+# </table>
+
+# Exercise 7 - Create a markdown cell introducing arithmetic expression examples
+
+# <p>Below are a few examples of evaluating arithmetic expressions in Python</p>
+
+# In[15]:
+
+
+a=(15+5)*2
+print(a)
+
+
+# In[16]:
+
+
+100+45-(20*2)/2
+
+
+# In[17]:
+
+
+45*(70-15)+5
+
+
+# Exercise 8 - Create a code cell to multiply and add numbers
+
+# In[18]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+a=(5*2)+7
+print(a)
+
+
+# Exercise 9 - Create a code cell to convert minutes to hours
+
+# In[19]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+minutes=200
+hours=200/60
+print(hours)
+
+
+# Exercise 10 - Insert a markdown cell to list Objectives
+
+# <h1>Some popular languages for Data Science</h1>
+# <ul>
+#     <li>Python</li>
+#     <li>SQL</li>
+#     <li>R</li>
+#     <li>Julia</li>
+#     <li>Scala</li>
+# </ul>
+
+# Exercise 11 - Create a markdown cell to indicate the Author’s name. (2 pts)
+
+# <h2>Author - Akshma Shalini Runda</h2>
+
+# In[ ]:
+
+
+
+
